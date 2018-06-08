399ce11305df39c79faab8e262ad4f2953bd6fa9
Евгений Ротарь <vagaabund@mail.ru>
2018-06-08 19:31:01 +0300

    Initial Commit

---
+++ TableMS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableMS
+//
+//  Created by Евгений Ротарь on 08.06.2018.
+//  Copyright © 2018 Евгений Ротарь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
